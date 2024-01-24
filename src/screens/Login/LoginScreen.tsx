14b423df1f602f1638946e6df5ed575b6172da27
Rohit Pakhrin <roitpak@gmail.com>
2024-01-24 17:42:35 +0545

    feat: Signup and sign in logic with email password validation

--- src/screens/Login/LoginScreen.tsx
+++ src/screens/Login/LoginScreen.tsx
@@ -1,11 +1,18 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  Button,
   SafeAreaView,
   StatusBar,
-  StyleSheet,
   Text,
+  TextInput,
+  TouchableOpacity,
+  View,
   useColorScheme,
 } from 'react-native';
+import {adminDashboardScreen} from '../../constants/Screens';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {styles} from './styles';
+import {ParamListBase, useNavigation} from '@react-navigation/native';
 // import authService from './appwrite/auth';
 
 function LoginScreen(): JSX.Element {
@@ -14,7 +21,23 @@ function LoginScreen(): JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? 'grey' : 'white',
   };
+
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+  const [email, setEmail] = useState('');
+  const [password, setPasswrod] = useState('');
+  const [validate, setValidated] = useState(false);
+  const [signupMode, setSignupMode] = useState(false);
+
   useEffect(() => {
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const passwordRegex = signupMode
+      ? /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+      : /^.{8,}$/;
+    if (emailRegex.test(email) && passwordRegex.test(password)) {
+      setValidated(true);
+    } else {
+      setValidated(false);
+    }
     // authService
     //   .createAccount({
     //     email: 'roitpak@gmail.com',
@@ -27,35 +50,45 @@ function LoginScreen(): JSX.Element {
     //   .catch(err => {
     //     console.log(err);
     //   });
-  });
+  }, [email, password, signupMode]);
+
+  const onPressLogin = () => {
+    navigation.navigate(adminDashboardScreen);
+  };
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <Text style={styles.highlight}>This is a login screen </Text>
+      <Text style={styles.header}>{signupMode ? 'Signup' : 'Login'}</Text>
+      <View style={styles.sectionContainer}>
+        <TextInput
+          value={email}
+          onChangeText={value => setEmail(value)}
+          style={styles.input}
+        />
+        <TextInput
+          value={password}
+          onChangeText={value => setPasswrod(value)}
+          secureTextEntry
+          style={styles.input}
+        />
+        <View style={styles.signupTextContainer}>
+          <Text style={styles.signupText}>
+            {signupMode ? 'Already have an account?' : 'Dont have an account?'}
+          </Text>
+          <TouchableOpacity onPress={() => setSignupMode(!signupMode)}>
+            <Text style={[styles.signupText, styles.signupTextHighlight]}>
+              {signupMode ? ' Sign in' : ' Signup'}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <Button disabled={!validate} title="Login" onPress={onPressLogin} />
+      </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default LoginScreen;
