d253a47ab96a443502e5c0f064d9c26610b1bb44
Rohit Pakhrin <roitpak@gmail.com>
2024-02-07 22:32:19 +0545

    feat: 17 home screen design

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {dashboardScreen, loginScreen} from '../../constants/Screens';
 import {ParamListBase, useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
@@ -10,9 +16,12 @@ import {useTheme} from '../../context/theme/useTheme';
 import Wrapper from '../../components/common/Wrapper';
 import CustomText from '../../components/common/CustomText';
 // import authService from './appwrite/auth';
+import strings from '../../constants/strings.json';
+import {Theme} from '../../constants/Types';
+import portrait from '../../assets/img/portrait.png';
 
 function HomeScreen(): JSX.Element {
-  const {changeTheme} = useTheme();
+  const {isDarkMode, changeTheme, theme} = useTheme();
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
 
   const goToLoginScreen = () => {
@@ -25,10 +34,30 @@ function HomeScreen(): JSX.Element {
   const {user} = useUser();
   return (
     <Wrapper>
-      <TouchableOpacity onPress={changeTheme}>
-        <Text>Change Theme</Text>
-      </TouchableOpacity>
-      <CustomText bold type="h1" title="Home Screen" />
+      <View style={styles(theme).container}>
+        <CustomText title={strings.name} type="h1" />
+        <TouchableOpacity onPress={changeTheme}>
+          <CustomText type="h2" title={isDarkMode ? 'Light>' : 'Dark>'} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles(theme).imageTextContainer}>
+        <View style={styles(theme).textContainer}>
+          <CustomText type="p1" title={strings.introMessage1} />
+          <CustomText type="h1" title={`${strings.name},`} />
+          <CustomText bold type="p1" title={strings.introMessage2} />
+        </View>
+        <Image
+          style={
+            Platform.OS === 'web' ? styles(theme).imageWeb : styles(theme).image
+          }
+          source={portrait}
+        />
+      </View>
+      <CustomText
+        style={styles(theme).introMessageStyle}
+        type="p1"
+        title={strings.introParagraph}
+      />
       {user && <CustomText type="h2" title={`Hi ${user.name}`} />}
       {!user && <Button title="Go to login" onPress={goToLoginScreen} />}
       <Button
@@ -39,5 +68,37 @@ function HomeScreen(): JSX.Element {
     </Wrapper>
   );
 }
+const styles = (theme: Theme) =>
+  StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    imageTextContainer: {
+      flexDirection: 'row',
+      marginTop: theme.sizes.large,
+      marginBottom: theme.sizes.extra_small,
+    },
+    textContainer: {
+      flex: 0.6,
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      justifyContent: 'flex-end',
+    },
+    image: {
+      flex: 1,
+      resizeMode: 'cover',
+    },
+    imageWeb: {
+      flex: 1,
+      height: 300,
+      resizeMode: 'contain',
+      alignSelf: 'flex-end',
+    },
+    introMessageStyle: {
+      textAlign: 'justify',
+    },
+  });
 
 export default HomeScreen;
