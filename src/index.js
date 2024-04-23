8385dd72a6cfc9b0abd385e852624ce40b4f2847
Rohit Pakhrin <roitpak@gmail.com>
2024-04-23 13:21:43 +0545

    Feat: MYB 33 publish to android (#32)
    * netlify no url fix
    
    * feat: MYB-33 publish to android

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('App', () => App);
-AppRegistry.runApplication('App', {rootTag: document.getElementById('root')});
+AppRegistry.registerComponent('RohitBlog', () => App);
+
+if (Platform.OS === 'web') {
+  AppRegistry.runApplication('RohitBlog', {
+    rootTag: document.getElementById('root'),
+  });
+}
