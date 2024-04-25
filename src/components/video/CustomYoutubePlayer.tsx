a06afe22bff9e5cf091c101ce440a9227c32973c
Rohit Pakhrin <roitpak@gmail.com>
2024-04-25 22:54:54 +0545

    chore: small design changes

--- src/components/video/CustomYoutubePlayer.tsx
+++ src/components/video/CustomYoutubePlayer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import YouTube, {YouTubeProps} from 'react-youtube';
+import {Dimensions} from '../../helpers/Dimensions';
 
 interface CustomYoutubePlayerProps {
   url: string;
@@ -11,8 +12,15 @@ function CustomYoutubePlayer({url}: CustomYoutubePlayerProps): JSX.Element {
     event.target.pauseVideo();
   };
   const opts: YouTubeProps['opts'] = {
-    height: 450,
-    width: 700,
+    // checking if on mobile device
+    height:
+      Dimensions.windowHeight > Dimensions.windowWidth
+        ? Dimensions.windowWidth * 0.45
+        : 450,
+    width:
+      Dimensions.windowHeight > Dimensions.windowWidth
+        ? Dimensions.windowWidth * 0.85
+        : 700,
     playerVars: {
       autoplay: 1,
     },
